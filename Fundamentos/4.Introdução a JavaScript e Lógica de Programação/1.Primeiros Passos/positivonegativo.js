305a136b4f62d9ed198ff76784908a58a1d4de99
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-07 18:38:10 -0300

    Exercício de positivo e negativo

---
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/1.Primeiros Passos/positivonegativo.js
@@ -0,0 +1,16 @@
+const a = -2;
+let resultado
+
+if (a > 0) {
+    resultado = "positive"
+}
+
+else if (a < 0) {
+    resultado = "negative"
+}
+
+else {
+    resultado = "zero"
+}
+
+console.log(resultado);
