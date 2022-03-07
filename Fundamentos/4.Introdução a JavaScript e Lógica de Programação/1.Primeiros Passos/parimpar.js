bd89847a502ef248cf28843ebdd53ec60dac15fe
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-07 19:13:42 -0300

    Exercício ParImpar JS

---
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/1.Primeiros Passos/parimpar.js
@@ -0,0 +1,13 @@
+let a = 6;
+let b = 2;
+let c = 4;
+let resultado;
+
+if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+    resultado = true
+}
+else {
+    resultado = false
+}
+
+console.log(resultado);
