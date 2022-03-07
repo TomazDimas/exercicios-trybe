5b20017241eb09776982cb6db3ec3bdd93b307b0
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-07 15:30:10 -0300

    Primeiro exercício de JS

---
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/1.Primeiros Passos/primeiroex.js
@@ -0,0 +1,10 @@
+const myName = "Tomaz";
+const birthCity = "Brasília";
+let birthYear = 2001;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
