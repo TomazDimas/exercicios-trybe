b03ec20b2ca8efb2b08c3f2146fee69103313dab
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-07 16:07:28 -0300

    Terceiro exercício de JS

---
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/1.Primeiros Passos/terceiroex.js
@@ -0,0 +1,11 @@
+const nota = 94;
+
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovado")
+}
+else if (nota < 60) {
+    console.log("Você foi reprovado")
+}
+else {
+    console.log("Você está na nossa lista de espera")
+}
