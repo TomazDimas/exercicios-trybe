8865bfd2eb2769c49a41b0b2fa44496669b14e72
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-08 20:10:26 -0300

    Exercício Bônus 3

--- Fundamentos/4.Introdução a JavaScript e Lógica de Programação/2.Array e loop For/exercicioarraybonus2.js
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/2.Array e loop For/exercicioarraybonus2.js
@@ -1,13 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multiplicados = [];
 
-for (let i = 1; i < numbers.length; i += 1) {
-  for (let x = 0; x < i; x += 1) {
-    if (numbers[i] > numbers[x]) {
-      let subs = numbers[i];
-      numbers[i] = numbers[x];
-      numbers[x] = subs;
-    }
+for (i = 0; i < numbers.length; i += 1) {
+  if (i === numbers.length - 1) {
+    let resultado = numbers[i] * 2;
+    multiplicados.push(resultado);
+  } else {
+    let resultado = numbers[i] * numbers[i + 1];
+    multiplicados.push(resultado);
   }
 }
 
-console.log(numbers);
+console.log(multiplicados);
