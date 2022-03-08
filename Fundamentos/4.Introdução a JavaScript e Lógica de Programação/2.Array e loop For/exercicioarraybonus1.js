730e360015f1c893de1b1b32b299556756ace974
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-08 20:01:44 -0300

    Exercício Bônus 1

---
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/2.Array e loop For/exercicioarraybonus1.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 1; i < numbers.length; i += 1) {
+  for (let x = 0; x < i; x += 1) {
+    if (numbers[i] < numbers[x]) {
+      let subs = numbers[i];
+      numbers[i] = numbers[x];
+      numbers[x] = subs;
+    }
+  }
+}
+
+console.log(numbers);
