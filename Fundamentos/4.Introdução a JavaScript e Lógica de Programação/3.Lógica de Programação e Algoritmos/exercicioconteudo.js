0046e0a3fa5f1ee6312695cf7506044daa61b710
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-09 16:24:31 -0300

    Exercício de conteúdo

--- Fundamentos/4.Introdução a JavaScript e Lógica de Programação/3.Lógica de Programação e Algoritmos/exercicioconteudo.js
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/3.Lógica de Programação e Algoritmos/exercicioconteudo.js
@@ -1,1 +1,12 @@
 let fruits = [3, 4, 10, 1, 12];
+let soma = 0;
+
+for (i = 0; i < fruits.length; i += 1) {
+  soma += fruits[i];
+}
+
+if (soma > 15) {
+  console.log(soma);
+} else {
+  console.log("valor menor que 16");
+}
