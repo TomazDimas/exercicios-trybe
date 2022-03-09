fdf56a5768fa218f86ee961d251a1cb17cdb191a
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-09 18:00:30 -0300

    Exercício em grupo

---
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/3.Lógica de Programação e Algoritmos/grupoaula.js
@@ -0,0 +1,45 @@
+// 1. Faça um algoritmo que calcule a soma de 50 a 100 usando a estrutura “for” e retorne no formato:
+// A soma de 50 a 100 é: X.
+// let soma = 0;
+// let array = [];
+
+// for (let i = 0; i <= 50; i += 1) {
+//     array.push(i + 50)
+// }
+
+// console.log(array);
+
+// for (let index = 0; index <= 50; index += 1) {
+//     soma += array[index]
+// }
+
+// for (let x = 50; x <= 100; x +=1) {
+//     soma += x;
+// }
+
+// console.log(soma);
+
+// 2. Crie um algoritmo que conte quantos números do intervalo entre 1 e 125 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta a sua escolha.
+
+// let contador = 0;
+
+// for (let i = 1; i <= 125; i += 1) {
+//     if (i % 3 == 0) {
+//         contador += 1;
+//     }
+// }
+
+// if (contador == 50) {
+//     console.log('Secreta');
+// }
+
+// console.log(contador);
+
+// 3. Crie um algoritmo que simula o jogo “pedra, papel e tesoura” levando em consideração que são apenas duas pessoas jogando e imprima o resultado no formato:
+// “Jogador 1 vence” ou “Empate” ou “Jogador 2 vence”.
+
+let jogador1 = 'tesoura'
+let jogador2 = 'pedra'
+let resultado;
+
