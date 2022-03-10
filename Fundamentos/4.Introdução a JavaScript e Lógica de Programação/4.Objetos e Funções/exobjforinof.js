15ff556acf8cddff4836cd22f16f5f77bc3590b1
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-10 19:44:15 -0300

    Exercício Objeto 5

--- Fundamentos/4.Introdução a JavaScript e Lógica de Programação/4.Objetos e Funções/exobjforinof.js
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/4.Objetos e Funções/exobjforinof.js
@@ -2,9 +2,34 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
   };
 
 // console.log('Ben vinda, ' + info.personagem);
 
 // info.recorrente = 'Sim';
 // console.log(info);
+
+// for (key in info) {
+//     console.log(key);
+// }
+
+// for (key in info) {
+//     console.log(info[key]);
+// }
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: "Sim"
+  };
+
+for (let key in info) {
+    if (info[key] === info2[key]) {
+        console.log('Ambos recorrentes');
+    }
+    else {
+        console.log(info[key] + ' e ' + info2[key]);
+    }
+}
