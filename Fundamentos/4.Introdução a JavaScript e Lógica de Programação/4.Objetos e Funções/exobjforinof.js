a8a7fb4d40ea1296c57871f7ac1fd610074f59b5
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-10 19:48:38 -0300

    Exercício Objeto 6

--- Fundamentos/4.Introdução a JavaScript e Lógica de Programação/4.Objetos e Funções/exobjforinof.js
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/4.Objetos e Funções/exobjforinof.js
@@ -1,9 +1,9 @@
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+//   };
 
 // console.log('Ben vinda, ' + info.personagem);
 
@@ -18,18 +18,33 @@ let info = {
 //     console.log(info[key]);
 // }
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
-    nota: 'O último MacPatinhas',
-    recorrente: "Sim"
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: "Sim"
+//   };
+
+// for (let key in info) {
+//     if (info[key] === info2[key]) {
+//         console.log('Ambos recorrentes');
+//     }
+//     else {
+//         console.log(info[key] + ' e ' + info2[key]);
+//     }
+// }
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
   };
 
-for (let key in info) {
-    if (info[key] === info2[key]) {
-        console.log('Ambos recorrentes');
-    }
-    else {
-        console.log(info[key] + ' e ' + info2[key]);
-    }
-}
+  console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
