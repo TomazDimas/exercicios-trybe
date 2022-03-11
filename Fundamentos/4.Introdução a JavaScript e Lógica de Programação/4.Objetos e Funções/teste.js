a86b2b233500bdac4c155b7026718be2f008c240
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-10 21:13:31 -0300

    Teste de comandos

---
+++ Fundamentos/4.Introdução a JavaScript e Lógica de Programação/4.Objetos e Funções/teste.js
@@ -0,0 +1,20 @@
+// let frutas = 'banana, laranja'.split(', ');
+// let palavra = frutas.join(', ');
+
+// console.log(frutas);
+// console.log(palavra);
+
+// frutas.reverse();
+
+// console.log(frutas);
+
+let nome = 'tomaz';
+let troca = nome.split('')
+troca.reverse();
+nome = troca.join('')
+
+
+
+console.log(nome);
+console.log(troca);
+console.log(troca.length);
