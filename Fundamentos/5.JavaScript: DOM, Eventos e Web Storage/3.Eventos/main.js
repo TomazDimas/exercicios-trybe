736e7bee99f4928d83722491de2782c8a1e8aab8
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-17 01:45:35 -0300

    Exercício de Conteúdo

--- Fundamentos/5.JavaScript: DOM, Eventos e Web Storage/3.Eventos/main.js
+++ Fundamentos/5.JavaScript: DOM, Eventos e Web Storage/3.Eventos/main.js
@@ -24,13 +24,29 @@ thirdLi.addEventListener('click', defineTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+function techText(event) {
+    let selecionado = document.querySelector('.tech')
+    selecionado.innerText = event.target.value;
+}
+
+input.addEventListener('input',  techText);
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener('dblclick', function () {
+    window.location.replace('https://blog.betrybe.com/');
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'red';
+  });
+
+myWebpage.addEventListener('mouseleave', function(event) {
+event.target.style.color = 'white';
+});
 
 // Segue abaixo um exemplo do uso de event.target:
 
