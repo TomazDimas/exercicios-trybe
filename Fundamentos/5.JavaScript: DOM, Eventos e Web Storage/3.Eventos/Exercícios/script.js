dac900d0c1b06b24154e73479f23261db192e269
Tomaz Dimas <tomazdimas2001@gmail.com>
2022-03-21 15:10:22 -0300

    Atualizações

--- Fundamentos/5.JavaScript: DOM, Eventos e Web Storage/3.Eventos/Exercícios/script.js
+++ Fundamentos/5.JavaScript: DOM, Eventos e Web Storage/3.Eventos/Exercícios/script.js
@@ -64,22 +64,152 @@ function createButton(nomeBotao) {
 createButton("Feriados");
 
 // EXERCICIO 3
-
-
 function colorButton() {
-  let vetorFeriados = document.querySelectorAll('.holiday');
-  let botaoFeriados = document.getElementById('btn-holiday');
+  let vetorFeriados = document.querySelectorAll(".holiday");
+  let botaoFeriados = document.getElementById("btn-holiday");
 
-  botaoFeriados.addEventListener('click', function() {
-  for (let i = 0; i < vetorFeriados.length; i += 1) {
-    let diaFeriado = vetorFeriados[i];
-    if (diaFeriado.style.background === 'blue') {
-      diaFeriado.style.background = 'rgb(238,238,238)'
-    } else {
-      diaFeriado.style.background = 'blue'
+  botaoFeriados.addEventListener("click", function () {
+    for (let i = 0; i < vetorFeriados.length; i += 1) {
+      let diaFeriado = vetorFeriados[i];
+      if (diaFeriado.style.background === "blue") {
+        diaFeriado.style.background = "rgb(238,238,238)";
+      } else {
+        diaFeriado.style.background = "blue";
+      }
     }
-  }
   });
 }
 
 colorButton();
+
+// EXERCICIO 4
+function criarBotaoDia(diaSemana) {
+  let botaoNovo = document.createElement("button");
+  let divBtnContainer = document.querySelector(".buttons-container");
+  botaoNovo.innerText = diaSemana;
+  botaoNovo.id = "btn-friday";
+  divBtnContainer.appendChild(botaoNovo);
+}
+
+criarBotaoDia("Sexta-feira");
+
+// EXERCICIO 5
+function changeFridayName() {
+  let botaoFriday = document.getElementById("btn-friday");
+  let vetorFridays = document.getElementsByClassName("friday");
+  let novoTexto = "Sextou BB 🍺";
+
+  botaoFriday.addEventListener("click", function () {
+    for (let i = 0; i < vetorFridays.length; i += 1) {
+      if (vetorFridays[i].innerText !== novoTexto) {
+        vetorFridays[i].innerText = novoTexto;
+      } else {
+        vetorFridays[i].innerText = valoresFridays[i];
+      }
+    }
+  });
+}
+
+let valoresFridays = [4, 11, 18, 25];
+changeFridayName();
+
+// EXERCICIO 6
+function zoomDias() {
+  let arrayDias = document.getElementById("days");
+
+  arrayDias.addEventListener("mouseover", function (event) {
+    event.target.style.fontSize = "30px";
+    event.target.style.fontWeight = "900";
+  });
+}
+
+function zoomOutDias() {
+  let arrayDias = document.getElementById("days");
+
+  arrayDias.addEventListener("mouseout", function (event) {
+    event.target.style.fontSize = "20px";
+    event.target.style.fontWeight = "500";
+  });
+}
+
+zoomDias();
+zoomOutDias();
+
+// EXERCICIO 7
+function criarTarefa(tarefa) {
+  let novaTarefa = document.createElement("span");
+  let divMyTasks = document.querySelector(".my-tasks");
+
+  novaTarefa.innerText = tarefa;
+  divMyTasks.appendChild(novaTarefa);
+}
+
+criarTarefa("Cozinhar");
+
+// EXERCICIO 8
+function creatColorLegend(cor) {
+  let novoDiv = document.createElement('div');
+  let divMyTasks = document.querySelector(".my-tasks");
+
+  novoDiv.classList = 'task';
+  novoDiv.style.background = cor;
+
+  divMyTasks.appendChild(novoDiv);
+}
+
+let corDecidida = 'blue'
+creatColorLegend(corDecidida);
+
+// EXERCICIO 9
+function taskSelected() {
+  let taskToSelect = document.querySelector('.task');
+  let selectedTask = document.getElementsByClassName('task selected')
+
+  taskToSelect.addEventListener('click', function(event) {
+    if (selectedTask.length === 0) {
+      event.target.classList = 'task selected'
+    } else {
+      event.target.classList = 'task'
+    }
+  })
+}
+
+taskSelected();
+
+// EXERCICIO 10
+function addEventMonth() {
+  let selectedTask = document.getElementsByClassName('task selected')
+  let diasMes = document.querySelector('#days');
+  let corOriginal = 'rgb(119,119,119)';
+  let taskColor = document.querySelector('.task').style.background;
+
+  diasMes.addEventListener('click', function(event) {
+    let targetColor = event.target.style.color;
+    if (selectedTask.length > 0 && targetColor !== taskColor) {
+      event.target.style.color = selectedTask[0].style.background;
+    } else if (selectedTask.length > 0 && targetColor === taskColor) {
+      event.target.style.color = corOriginal;
+    }
+  })
+}
+
+addEventMonth();
+
+// EXERCICIO BÔNUS
+function addNewTask() {
+  let inputTxt = document.querySelector('#task-input');
+  let taskList = document.querySelector('.task-list');
+  let addBtn = document.getElementById('btn-add');
+  
+  addBtn.addEventListener('click', function(){
+    if (inputTxt.value.length > 0) {
+      let newTask = document.createElement('li');
+      newTask.innerText = inputTxt.value
+      taskList.appendChild(newTask);
+    } else {
+      window.alert('Espaço Incompleto');
+    }
+  })
+}
+
+addNewTask();
