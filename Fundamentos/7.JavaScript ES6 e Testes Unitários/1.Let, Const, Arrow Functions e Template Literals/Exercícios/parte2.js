1b8cb5b8f02d47de2f8eae5e283bc48c20c5cba2
TomazDimas <tomazdimas2001@gmail.com>
2022-04-04 19:32:57 -0300

    Parte 2 Ex 4

--- Fundamentos/7.JavaScript ES6 e Testes Unitários/1.Let, Const, Arrow Functions e Template Literals/Exercícios/parte2.js
+++ Fundamentos/7.JavaScript ES6 e Testes Unitários/1.Let, Const, Arrow Functions e Template Literals/Exercícios/parte2.js
@@ -21,3 +21,26 @@ const longestWord = frase => {
 
 // console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));
 
+// FUNÇÃO 4
+const arrayEx = ['HTML', 'CSS', 'Git', 'Unix', 'Bash'];
+
+const findX = (frase, subs) => {
+    let array = frase.split(' ');
+    for (let i in array) {
+        array[i] === 'x' ? array[i] = subs : array[i] = array[i];
+    }
+    array = array.join(' ');
+    return array;
+}
+
+const concatAll = string => {
+    const sortedArray = arrayEx.sort();
+    return `${string}! Minhas cinco principais habilidades são:
+    -${sortedArray[0]}
+    -${sortedArray[1]}
+    -${sortedArray[2]}
+    -${sortedArray[3]}
+    -${sortedArray[4]}`
+}
+
+console.log(concatAll(findX('Tryber x aqui', 'tomaz')));
