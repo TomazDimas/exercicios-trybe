6625704d36d5b2b044cb666ce8e4a6dd72dd5940
TomazDimas <tomazdimas2001@gmail.com>
2022-04-06 17:42:33 -0300

    Ex 3 Item 2

--- Fundamentos/7.JavaScript ES6 e Testes Unitários/3.Jest/Exercícios/myFizzBuzz..test.js
+++ Fundamentos/7.JavaScript ES6 e Testes Unitários/3.Jest/Exercícios/myFizzBuzz..test.js
@@ -3,3 +3,7 @@ const myFizzBuzz = require('./myFizzBuzz');
 test('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
   expect(myFizzBuzz(15)).toBe('fizzbuzz');
 })
+
+test('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+  expect(myFizzBuzz(9)).toBe('fizz');
+})
