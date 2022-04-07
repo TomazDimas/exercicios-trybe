3edd1f0dcec19d71db5f4458b32e3b2da1757ed7
TomazDimas <tomazdimas2001@gmail.com>
2022-04-07 16:15:17 -0300

    Exercício 1

---
+++ Fundamentos/8.High Order Functions/1.Introdução a HOF/Exercício/ex1.js
@@ -0,0 +1,15 @@
+const createEmployee = (nome) => {
+  const emailName = nome.toLowerCase().split(' ').join('_')
+  return {nomeCompleto: nome, email: `${emailName}@trybe.com`}
+};
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(createEmployee));
