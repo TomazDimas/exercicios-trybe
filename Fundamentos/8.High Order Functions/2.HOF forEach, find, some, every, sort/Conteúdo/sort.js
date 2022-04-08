2c7219127274d10b60adc3308ae0effd72908b2e
TomazDimas <tomazdimas2001@gmail.com>
2022-04-08 16:02:41 -0300

    Conteúdo sort Ex 1

---
+++ Fundamentos/8.High Order Functions/2.HOF forEach, find, some, every, sort/Conteúdo/sort.js
@@ -0,0 +1,13 @@
+// EXERCÍCIO 1
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Adicione se código aqui
+people.sort((personB, personA) => personB.age - personA.age);
+
+console.log(people);
