125704efee5e68c1a91f238d5092b36d4157981c
TomazDimas <tomazdimas2001@gmail.com>
2022-04-08 15:51:41 -0300

    Conteúdo someEvery Ex 2

--- Fundamentos/8.High Order Functions/2.HOF forEach, find, some, every, sort/Conteúdo/someAndEvery.js
+++ Fundamentos/8.High Order Functions/2.HOF forEach, find, some, every, sort/Conteúdo/someAndEvery.js
@@ -1,3 +1,4 @@
+// EXERCÍCIO 1
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
 const hasName = (arr, name) => {
@@ -6,3 +7,19 @@ const hasName = (arr, name) => {
 }
 
 console.log(hasName(names, 'Ana'))
+
+// EXERCÍCIO 2
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  //Adicione seu código aqui
+  return arr.every((currentAgr) => currentAgr.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 18));
