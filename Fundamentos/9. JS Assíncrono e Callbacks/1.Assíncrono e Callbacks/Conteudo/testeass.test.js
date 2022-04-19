4ec609d8577b74316ed641b4e508374cd7efebeb
TomazDimas <tomazdimas2001@gmail.com>
2022-04-19 16:14:13 -0300

    Conteúdo Teste Assíncrono 2

--- Fundamentos/9. JS Assíncrono e Callbacks/1.Assíncrono e Callbacks/Conteudo/testeass.test.js
+++ Fundamentos/9. JS Assíncrono e Callbacks/1.Assíncrono e Callbacks/Conteudo/testeass.test.js
@@ -1,11 +1,29 @@
-test('Não deveria passar!', (done) => {
+// test('Não deveria passar!', (done) => {
+//   setTimeout(() => {
+//     try {
+//       expect(10).toBe(5);
+//       console.log('Deveria falhar!');
+//       done();
+//     } catch (error) {
+//       done(error);
+//     }
+//   }, 500);
+// });
+
+const asyncSum = (a, b, callback) => {
   setTimeout(() => {
+    const result = a + b + 1;
+    callback(result);
+  }, 500);
+};
+
+test('Testando asyncSum, soma 5 mais 10', (done) => {
+  asyncSum(5, 10, (result) => {
     try {
-      expect(10).toBe(5);
-      console.log('Deveria falhar!');
+      expect(result).toBe(15);
       done();
     } catch (error) {
       done(error);
     }
-  }, 500);
+  });
 });
