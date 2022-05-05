b3cff2cbd5a35114bcd610efac8a1e886bc32ee2
TomazDimas <tomazdimas2001@gmail.com>
2022-05-05 19:01:25 -0300

    Exercício 2

--- Front-End/1.Introdução a React/1.Hello World no React/exercicios-10.1/src/App.js
+++ Front-End/1.Introdução a React/1.Hello World no React/exercicios-10.1/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const arrayTasks = ['estudar', 'malhar', 'correr', 'dormir'];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      { arrayTasks.map((task) => Task(task)) }
+    </ul>
   );
 }
 
