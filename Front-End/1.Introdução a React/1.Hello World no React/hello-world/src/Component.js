c382b931c2f88fc3e9887668bda3b28320e898a1
TomazDimas <tomazdimas2001@gmail.com>
2022-05-05 18:20:56 -0300

    Exercícios de Conteúdo

---
+++ Front-End/1.Introdução a React/1.Hello World no React/hello-world/src/Component.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Component extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Tomaz Dimas</h1>
+        <p>Olá! Meu nome é Tomaz, tenho 20 anos e sou apaixonado por tecnologia!</p>
+      </div>
+    )
+  }
+}
+
+export default Component
