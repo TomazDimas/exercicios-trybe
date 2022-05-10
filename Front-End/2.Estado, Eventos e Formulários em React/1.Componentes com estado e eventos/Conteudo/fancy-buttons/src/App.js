ebb7574541527717f171cd38cf3f66a2c9bb435c
TomazDimas <tomazdimas2001@gmail.com>
2022-05-10 18:33:36 -0300

    Conteúdo Intro Eventos 1

--- Front-End/2.Estado, Eventos e Formulários em React/1.Componentes com estado e eventos/Conteudo/fancy-buttons/src/App.js
+++ Front-End/2.Estado, Eventos e Formulários em React/1.Componentes com estado e eventos/Conteudo/fancy-buttons/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+
+    function handleClick() {
+      console.log('oi');
+    }
+
+    return(
+      <div>
+        <button onClick={handleClick}>Botão</button>
+      </div>
+    )
+  }
 }
 
 export default App;
