b3a44e571e42663173687fd647562b012baa952c
TomazDimas <tomazdimas2001@gmail.com>
2022-05-10 19:12:12 -0300

    Conteúdo This e Bind no Constructor 3 e 4

--- Front-End/2.Estado, Eventos e Formulários em React/1.Componentes com estado e eventos/Conteudo/fancy-buttons/src/App.js
+++ Front-End/2.Estado, Eventos e Formulários em React/1.Componentes com estado e eventos/Conteudo/fancy-buttons/src/App.js
@@ -2,27 +2,36 @@ import './App.css';
 import React from 'react';
 
 class App extends React.Component {
+  
+  constructor() {
+    super()
+    this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+  }
+
+  handleClick1() {
+    console.log('oi');
+    console.log(this);
+  }
+
+
+  handleClick2() {
+    console.log('olá');
+    console.log(this);
+  }
+
+
+  handleClick3 = () => {
+    console.log('tudo bom?');
+    console.log(this);
+  }
+
   render() {
-
-    function handleClick1() {
-      console.log('oi');
-    }
-
-
-    function handleClick2() {
-      console.log('olá');
-    }
-
-
-    function handleClick3() {
-      console.log('tudo bom?');
-    }
-
     return(
       <div>
-        <button onClick={handleClick1}>Botão1</button>
-        <button onClick={handleClick2}>Botão2</button>
-        <button onClick={handleClick3}>Botão3</button>
+        <button onClick={this.handleClick1}>Botão1</button>
+        <button onClick={this.handleClick2}>Botão2</button>
+        <button onClick={this.handleClick3}>Botão3</button>
       </div>
     )
   }
