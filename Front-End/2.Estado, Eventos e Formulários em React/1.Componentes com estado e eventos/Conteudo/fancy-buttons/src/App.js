b24609c2c55cf88c6bc868caf7815e6ef4eb5375
TomazDimas <tomazdimas2001@gmail.com>
2022-05-10 18:35:28 -0300

    Conteúdo Intro Eventos 2

--- Front-End/2.Estado, Eventos e Formulários em React/1.Componentes com estado e eventos/Conteudo/fancy-buttons/src/App.js
+++ Front-End/2.Estado, Eventos e Formulários em React/1.Componentes com estado e eventos/Conteudo/fancy-buttons/src/App.js
@@ -4,13 +4,25 @@ import React from 'react';
 class App extends React.Component {
   render() {
 
-    function handleClick() {
+    function handleClick1() {
       console.log('oi');
+    }
+
+
+    function handleClick2() {
+      console.log('olá');
+    }
+
+
+    function handleClick3() {
+      console.log('tudo bom?');
     }
 
     return(
       <div>
-        <button onClick={handleClick}>Botão</button>
+        <button onClick={handleClick1}>Botão1</button>
+        <button onClick={handleClick2}>Botão2</button>
+        <button onClick={handleClick3}>Botão3</button>
       </div>
     )
   }
