2dbc700c4fdf49b598e81b14a5d0a6d52681249f
TomazDimas <tomazdimas2001@gmail.com>
2022-05-11 14:53:50 -0300

    Conteúdo Unindo componentes 5, 6 e 7

--- Front-End/2.Estado, Eventos e Formulários em React/1.Componentes com estado e eventos/Conteudo/fancy-buttons/src/App.js
+++ Front-End/2.Estado, Eventos e Formulários em React/1.Componentes com estado e eventos/Conteudo/fancy-buttons/src/App.js
@@ -7,23 +7,41 @@ class App extends React.Component {
     super()
     this.handleClick1 = this.handleClick1.bind(this)
     this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+    this.state = {
+      contagemBotao1: 0,
+      contagemBotao2: 0,
+      contagemBotao3: 0,
+    }
   }
 
   handleClick1() {
     console.log('oi');
     console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      contagemBotao1: estadoAnterior.contagemBotao1 + 1,
+    }))
+    console.log(this.state);
   }
 
 
   handleClick2() {
     console.log('olá');
     console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      contagemBotao2: estadoAnterior.contagemBotao2 + 1,
+    }))
+    console.log(this.state);
   }
 
 
   handleClick3 = () => {
     console.log('tudo bom?');
     console.log(this);
+    this.setState((estadoAnterior, _props) => ({
+      contagemBotao3: estadoAnterior.contagemBotao3 + 1,
+    }))
+    console.log(this.state);
   }
 
   render() {
