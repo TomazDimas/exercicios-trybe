74236832cfdfdc8278a1bff899733edafdd2c9e3
TomazDimas <tomazdimas2001@gmail.com>
2022-06-07 17:30:27 -0300

    Exercicio 1 Item 2

--- Front-End/4.Testes automatizados com RTL/2.RLT - Mocks e Inputs/Exercicio1/service.test.js
+++ Front-End/4.Testes automatizados com RTL/2.RLT - Mocks e Inputs/Exercicio1/service.test.js
@@ -2,6 +2,8 @@ const service = require('./service');
 
 describe('Exercício 1', () => {
 
+  // afterEach(() => service.randomNumber.mockRestore())
+
   it('Exercício 1 Item 1', () => {
     jest.spyOn(service, 'randomNumber').mockReturnValue(10)
 
@@ -9,4 +11,21 @@ describe('Exercício 1', () => {
     expect(service.randomNumber).toHaveBeenCalled()
     expect(service.randomNumber).toHaveBeenCalledTimes(1)
   })
+
+  it('Exercício 1 Item 2', () => {
+    service.randomNumber = jest.fn().mockImplementation((a, b) => a / b) 
+    
+    console.log(service.randomNumber(6, 3));
+    expect(service.randomNumber(10, 2)).toBe(5)
+    expect(service.randomNumber).toHaveBeenCalled()
+    expect(service.randomNumber).toHaveBeenCalledTimes(2)
+    expect(service.randomNumber).toHaveBeenCalledWith(10, 2)
+  })
+
+  it('Exercício 1 Item 3', () => {
+    service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c) 
+    
+    console.log(service.randomNumber(6, 3, 2));
+  })
+
 })
