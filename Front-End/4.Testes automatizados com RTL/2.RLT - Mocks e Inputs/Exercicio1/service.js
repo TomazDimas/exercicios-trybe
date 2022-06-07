7737dcca54e8923cc515c6eab4643152f8037d13
TomazDimas <tomazdimas2001@gmail.com>
2022-06-07 17:42:22 -0300

    Exercício 1 Item 3

--- Front-End/4.Testes automatizados com RTL/2.RLT - Mocks e Inputs/Exercicio1/service.js
+++ Front-End/4.Testes automatizados com RTL/2.RLT - Mocks e Inputs/Exercicio1/service.js
@@ -3,4 +3,18 @@ const randomNumber = () => {
   return aleatorio;
 }
 
-module.exports = { randomNumber };
+const toUpperCase = (string) => {
+  const uppered = string.toUpperCase();
+  return uppered
+}
+
+const firstLetter = (string) => {
+  const wordArray = string.split('');
+  return wordArray[0]
+}
+
+const concatString = (str1, str2) => {
+  return `${str1}${str2}`
+}
+
+module.exports = { randomNumber, toUpperCase, firstLetter, concatString };
--- Front-End/4.Testes automatizados com RTL/2.RLT - Mocks e Inputs/Exercicio1/service.test.js
+++ Front-End/4.Testes automatizados com RTL/2.RLT - Mocks e Inputs/Exercicio1/service.test.js
@@ -1,6 +1,6 @@
 const service = require('./service');
 
-describe('Exercício 1', () => {
+describe('Exercício 1 Itens 1 a 3', () => {
 
   // afterEach(() => service.randomNumber.mockRestore())
 
@@ -15,17 +15,31 @@ describe('Exercício 1', () => {
   it('Exercício 1 Item 2', () => {
     service.randomNumber = jest.fn().mockImplementation((a, b) => a / b) 
     
-    console.log(service.randomNumber(6, 3));
     expect(service.randomNumber(10, 2)).toBe(5)
     expect(service.randomNumber).toHaveBeenCalled()
-    expect(service.randomNumber).toHaveBeenCalledTimes(2)
+    expect(service.randomNumber).toHaveBeenCalledTimes(1)
     expect(service.randomNumber).toHaveBeenCalledWith(10, 2)
   })
 
-  it('Exercício 1 Item 3', () => {
+  it('Exercício 1 Item 3.1', () => {
     service.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c) 
     
-    console.log(service.randomNumber(6, 3, 2));
+    expect(service.randomNumber(10, 2, 5)).toBe(100)
+    expect(service.randomNumber).toHaveBeenCalled()
+    expect(service.randomNumber).toHaveBeenCalledTimes(1)
+    expect(service.randomNumber).toHaveBeenCalledWith(10, 2, 5)
+  })
+
+  it('Exercício 1 Item 3.2', () => {
+    service.randomNumber.mockReset()
+    service.randomNumber = jest.fn().mockImplementation((a) => 2 * a) 
+    
+    expect(service.randomNumber(10)).toBe(20)
+    expect(service.randomNumber).toHaveBeenCalled()
+    expect(service.randomNumber).toHaveBeenCalledTimes(1)
+    expect(service.randomNumber).toHaveBeenCalledWith(10)
   })
 
 })
+
+describe('Exercício 1 Item 4')
