d3ab38d884fe3ed989390f7432b83e2261b00587
TomazDimas <tomazdimas2001@gmail.com>
2022-06-08 18:49:16 -0300

    Exercício 2 Item 3

--- Front-End/5.Gerenciamento de estado com Redux/1.Introdução ao Redux/Exercicio2/script.js
+++ Front-End/5.Gerenciamento de estado com Redux/1.Introdução ao Redux/Exercicio2/script.js
@@ -1,3 +1,55 @@
+const ESTADO_INICIAL_1 = {
+  nome: 'Rodrigo',
+  sobrenome: 'Santos da Silva',
+  endereco: 'Rua Soldado Mathias, 765',
+  cidade: 'Belo Horizonte',
+};
+
+const ESTADO_INICIAL_2 = {
+  nome: 'Bruna',
+  sobrenome: 'Santana Oliveira',
+  endereco: 'Rua Holanda, 332',
+  cidade: 'São Paulo',
+};
+
+const changeNames1 = (nome, sobrenome) => ({
+  type: 'CHANGE_NAMES_1',
+  nome,
+  sobrenome,
+})
+
+const changeNames2 = (nome, sobrenome) => ({
+  type: 'CHANGE_NAMES_2',
+  nome,
+  sobrenome,
+})
+
+const meuPrimeiroReducer = (state = ESTADO_INICIAL_1, action) => {
+  switch (action.type) {
+    case 'CHANGE_NAMES_1':
+      return {
+        ...state,
+        nome: action.nome,
+        sobrenome: action.sobrenome
+      }
+    default:
+      return state;
+  }
+};
+
+const meuSegundoReducer = (state = ESTADO_INICIAL_2, action) => {
+  switch (action.type) {
+    case 'CHANGE_NAMES_2':
+      return {
+        ...state,
+        nome: action.nome,
+        sobrenome: action.sobrenome
+      }
+    default:
+      return state;
+  }
+};
+
 const reducesCombinados = Redux.combineReducers({
   meuPrimeiroReducer,
   meuSegundoReducer
