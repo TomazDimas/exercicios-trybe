2071ac235308823a5ff821c135b038e15e47f13c
TomazDimas <tomazdimas2001@gmail.com>
2022-08-22 21:28:32 -0300

    Exercício 5

--- Back-End/4.Desenvolvimento Web com Node.js/1.Runtime Assíncrono/Exercicios/pacote-node/bmi.js
+++ Back-End/4.Desenvolvimento Web com Node.js/1.Runtime Assíncrono/Exercicios/pacote-node/bmi.js
@@ -1,6 +1,6 @@
 const readline = require('readline-sync');
 
-const altura = readline.questionInt('Whats your height?');
+const altura = readline.questionFloat('Whats your height?');
 const peso = readline.questionInt('What your weight?');
 
 const calculateBMI = (alt, peso) => {
@@ -12,6 +12,32 @@ const calculateBMI = (alt, peso) => {
 const main = () => {
   const bmi = calculateBMI(altura, peso);
   console.log(`Seu IMC é ${bmi}`);
+  if (bmi < 18.5) {
+    console.log('Status: Underweight (thin)');
+    return;
+  }
+
+  if (bmi >= 18.5 && bmi < 25) {
+    console.log('Status: Normal weight');
+    return;
+  }
+
+  if (bmi >= 25 && bmi < 30) {
+    console.log('Status: Overweight (overweight)');
+    return;
+  }
+
+  if (bmi >= 30 && bmi < 35) {
+    console.log('Status: Grade I obesity');
+    return;
+  }
+
+  if (bmi >= 35 && bmi < 40) {
+    console.log('Status: Grade II obesity');
+    return;
+  }
+
+  console.log('Status: Obesity grades III and IV');
 };
 
 main();
