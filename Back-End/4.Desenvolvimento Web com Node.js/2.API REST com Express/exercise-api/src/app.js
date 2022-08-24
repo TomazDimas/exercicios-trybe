6449009f6014b221265437030a45b5b6d36e4ffa
TomazDimas <tomazdimas2001@gmail.com>
2022-08-24 20:05:30 -0300

    Exercícios 1 a 4

---
+++ Back-End/4.Desenvolvimento Web com Node.js/2.API REST com Express/exercise-api/src/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+app.get('/myActivities/:id', (req, res) => {
+  const { id } = req.params;
+  const activitie = activities.find((atv) => atv.id === Number(id));
+
+  res.status(200).json(activitie);
+})
+
+module.exports = app;
---
+++ Back-End/4.Desenvolvimento Web com Node.js/2.API REST com Express/exercise-api/src/server.js
@@ -0,0 +1,3 @@
+const app = require('./app');
+
+app.listen(3002, () => console.log('Server aberto em 3002'));
