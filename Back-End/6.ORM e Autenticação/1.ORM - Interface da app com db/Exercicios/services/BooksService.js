4978899982070fba158ba8eed457c4849c7f07ad
TomazDimas <tomazdimas2001@gmail.com>
2022-10-12 20:40:47 -0300

    Exercício 9

--- Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/services/BooksService.js
+++ Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/services/BooksService.js
@@ -1,16 +1,22 @@
-const { Book } = require('../models');
+const { Book } = require("../models");
 
 const getAll = async () => {
   const books = await Book.findAll();
   return books;
-}
+};
 
 const getById = async (id) => {
   const books = await Book.findByPk(id);
   return books;
-}
+};
+
+const create = async ({ title, author, pageQuantity }) => {
+  const newBook = await Book.create({ title, author, pageQuantity });
+  return newBook;
+};
 
 module.exports = {
   getAll,
   getById,
-}
+  create,
+};
