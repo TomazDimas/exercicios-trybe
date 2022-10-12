aa38309921321032b3edf8bc1dd1c430ab9569b9
TomazDimas <tomazdimas2001@gmail.com>
2022-10-12 20:56:19 -0300

    Exercício 13

--- Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/controllers/BookController.js
+++ Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/controllers/BookController.js
@@ -17,13 +17,27 @@ const create = async (req, res) => {
   const { title, author, pageQuantity } = req.body;
   if (!title || !author || !pageQuantity)
     return res.status(404).json({ message: "Campo faltando" });
-  const newBook = await BookService.create({ title, author, pageQuantity })
+  const newBook = await BookService.create({ title, author, pageQuantity });
 
   res.status(200).json(newBook);
+};
+
+const update = async (req, res) => {
+  const { id } = req.params;
+  const { title, author, pageQuantity } = req.body;
+  const updatedBook = await BookService.update(id, {
+    title,
+    author,
+    pageQuantity,
+  });
+  if (!updatedBook) return res.status(400).json({ message: "Book not found" });
+
+  res.status(200).json({ message: "Book updated", book: updatedBook });
 };
 
 module.exports = {
   getAll,
   getById,
   create,
+  update,
 };
