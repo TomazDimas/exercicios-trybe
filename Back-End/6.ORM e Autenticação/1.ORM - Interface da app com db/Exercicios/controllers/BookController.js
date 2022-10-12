5ace9119eaa0706335d4bec816421fe4e8f68f70
TomazDimas <tomazdimas2001@gmail.com>
2022-10-12 20:37:27 -0300

    Exercício 7

--- Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/controllers/BookController.js
+++ Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/controllers/BookController.js
@@ -1,10 +1,18 @@
-const BookService = require('../services/BooksService');
+const BookService = require("../services/BooksService");
 
 const getAll = async (req, res) => {
   const books = await BookService.getAll();
   res.status(200).json(books);
-}
+};
+
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const books = await BookService.getById(id);
+  if (!books) return res.status(404).json({ message: "Book not found" });
+
+  res.status(200).json(books);
+};
 
 module.exports = {
   getAll,
-}
+};
