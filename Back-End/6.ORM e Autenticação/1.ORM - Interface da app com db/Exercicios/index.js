1d15544c2241ae35f1faffcb4e595fc5d23edf74
TomazDimas <tomazdimas2001@gmail.com>
2022-10-12 20:56:36 -0300

    Exercício 14

--- Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/index.js
+++ Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/index.js
@@ -1,15 +1,17 @@
-const BookController = require('./controllers/BookController');
+const BookController = require("./controllers/BookController");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 4001;
 
 app.use(express.json());
 
-app.get('/books', BookController.getAll);
+app.get("/books", BookController.getAll);
 
-app.get('/books/:id', BookController.getById);
+app.get("/books/:id", BookController.getById);
 
-app.post('/books', BookController.create);
+app.post("/books", BookController.create);
+
+app.put("/books", BookController.update);
 
 app.listen(PORT, () => console.log(`RODANDO APP EM: ${PORT}`));
