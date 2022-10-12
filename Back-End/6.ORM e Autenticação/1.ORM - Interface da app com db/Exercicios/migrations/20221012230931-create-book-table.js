f237dea30e82fd9ec31c9473c918cd7bae4f8af5
TomazDimas <tomazdimas2001@gmail.com>
2022-10-12 20:19:05 -0300

    Exercício 2

--- Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/migrations/20221012230931-create-book-table.js
+++ Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/migrations/20221012230931-create-book-table.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       pageQuantity: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.INTEGER,
       },
       createdAt: {
