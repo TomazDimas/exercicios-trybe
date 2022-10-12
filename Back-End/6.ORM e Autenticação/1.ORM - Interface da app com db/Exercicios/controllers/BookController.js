e6c2a8dfcb3dd25c912e85ef8045bbf901762f26
TomazDimas <tomazdimas2001@gmail.com>
2022-10-12 20:43:21 -0300

    Requisito 10

--- Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/controllers/BookController.js
+++ Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/controllers/BookController.js
@@ -13,6 +13,15 @@ const getById = async (req, res) => {
   res.status(200).json(books);
 };
 
+const create = async (req, res) => {
+  const { title, author, pageQuantity } = req.body;
+  if (!title || !author || !pageQuantity)
+    return res.status(404).json({ message: "Campo faltando" });
+  const newBook = BookService.create({ title, author, pageQuantity })
+
+  res.statu(200).json(newBook);
+};
+
 module.exports = {
   getAll,
   getById,
