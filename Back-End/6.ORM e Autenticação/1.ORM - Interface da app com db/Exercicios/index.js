e99acb7c329894de1670210b476f8c70ced98a44
TomazDimas <tomazdimas2001@gmail.com>
2022-10-12 21:03:08 -0300

    Exercício 17

--- Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/index.js
+++ Back-End/6.ORM e Autenticação/1.ORM - Interface da app com db/Exercicios/index.js
@@ -14,6 +14,6 @@ app.post("/books", BookController.create);
 
 app.put("/books/:id", BookController.update);
 
-app.delete("/books/:id", BookController.delete);
+app.delete("/books/:id", BookController.remove);
 
 app.listen(PORT, () => console.log(`RODANDO APP EM: ${PORT}`));
