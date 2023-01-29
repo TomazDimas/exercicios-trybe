b075039bfb20e868c1d4bc2308bc99c6ca50c512
TomazDimas <tomazdimas2001@gmail.com>
2023-01-29 14:06:44 -0300

    Exercício 2

---
+++ CS/2.Padrões de Projeto/2.Padrões - Iterator, Adapter, Strategy/exercicio2.py
@@ -0,0 +1,49 @@
+from collections.abc import Iterable, Iterator
+
+
+class Carta:
+    def __init__(self, valor, naipe):
+        self.valor = valor
+        self.naipe = naipe
+
+    def __repr__(self):
+        return '<%s de %s>' % (self.valor, self.naipe)
+
+class Baralho(Iterable):
+    naipes = 'copas ouros espadas paus'.split()
+    valores = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split()
+
+    def __init__(self):
+        self._cartas = [
+            Carta(valor, naipe)
+            for naipe in self.naipes
+            for valor in self.valores
+        ]
+
+    def getCards(self):
+        return self._cartas
+
+    def __len__(self):
+        return len(self._cartas)
+
+    def __iter__(self):
+        return BaralhoIterator(self._cartas)
+
+class BaralhoIterator(Iterator):
+    def __init__(self, iterable):
+        self.__iterable = iterable
+        self.__index = 0
+
+    def __next__(self):
+        try:
+            result = self.__iterable[self.__index]
+        except IndexError:
+            raise StopIteration
+
+        self.__index += 1
+        return result
+
+
+baralho = Baralho()
+for carta in baralho:
+    print(carta)
