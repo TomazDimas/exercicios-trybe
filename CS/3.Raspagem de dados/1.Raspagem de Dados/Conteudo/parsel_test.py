e7f45a16c000e30ee71ed990e73543272ea4c865
TomazDimas <tomazdimas2001@gmail.com>
2023-02-03 18:48:57 -0300

    Conteúdo - Recrusos Paginados

--- CS/3.Raspagem de dados/1.Raspagem de Dados/Conteudo/parsel_test.py
+++ CS/3.Raspagem de dados/1.Raspagem de Dados/Conteudo/parsel_test.py
@@ -1,7 +1,7 @@
 from parsel import Selector
 import requests
 
-site_url = "http://books.toscrape.com/"
+site_url = "http://books.toscrape.com/catalogue/"
 response = requests.get(site_url)
 selector =Selector(text=response.text)
 
@@ -11,4 +11,22 @@ book_prices = selector.css(".product_price .price_color::text").getall()
 for product in selector.css(".product_pod"):
     title = product.css("h3 a::text").get()
     price = product.css(".product_price .price_color::text").get()
-    print(title, price)
+    # print(title, price)
+
+next_page_url = selector.css(".next a::attr(href)").get()
+# print(next_page_url)
+
+# URL_BASE = "http://books.toscrape.com/catalogue/"
+next_page_url = 'page-1.html'
+while next_page_url:
+    # Busca o conteúdo da próxima página
+    response = requests.get(site_url + next_page_url)
+    selector = Selector(text=response.text)
+    # Imprime os produtos de uma determinada página
+    print()
+    for product in selector.css(".product_pod"):
+        title = product.css("h3 a::text").get()
+        price = product.css(".product_price .price_color::text").get()
+        print(title, price)
+    # Descobre qual é a próxima página
+    next_page_url = selector.css(".next a::attr(href)").get()
